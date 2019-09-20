616c5cf6c253ff7ad987d4ec7dd0624a7ea19b67
Melissa Williams <melissa@brandmeetscopy.com>
2019-09-20 04:21:56 +0000

    Automatically backed up by Learn

--- db/migrate/20190920042129_create_students.rb
+++ db/migrate/20190920042129_create_students.rb
@@ -1,7 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.0]
   def change
     create_table :students do |t|
-      t.string :first_name 
+      t.string :first_name
       t.string :last_name
     end
   end
